2c212d92878f2925e06b852585e14801398fa712
Brandon Lee <brandononlyu@gmail.com>
2022-05-29 02:54:17 -0400

    Update TimingHelpers documentation

--- VOCALOIDParser/Base/TimingHelpers.cs
+++ VOCALOIDParser/Base/TimingHelpers.cs
@@ -2,13 +2,26 @@
 {
     public class TimingHelpers
     {
+        /// <summary>
+        /// How many ticks are in a quarter note. This is specific to VOCALOID5, and should be the same across clients.
+        /// </summary>
         public const int TICKS_PER_BEAT = 480;
 
+        /// <summary>
+        /// Converts quarter note beats to ticks
+        /// </summary>
+        /// <param name="time">The quarter note position</param>
+        /// <returns>The tick position</returns>
         public static int BeatToTick(float time)
         {
             return (int)(time * TICKS_PER_BEAT);
         }
 
+        /// <summary>
+        /// Converts ticks to quarter note beats
+        /// </summary>
+        /// <param name="tick">The tick position</param>
+        /// <returns>The quarter note position</returns>
         public static float TickToBeat(int tick)
         {
             return (float)tick / TICKS_PER_BEAT;
