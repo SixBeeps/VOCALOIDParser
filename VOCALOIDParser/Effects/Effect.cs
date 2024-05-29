8d933a16b61e0220d55ad90c13b24b6de0246e43
Brandon Lee <brandononlyu@gmail.com>
2024-05-29 19:15:47 -0400

    Dictionarify Effect.FromJsonEffect

--- VOCALOIDParser/Effects/Effect.cs
+++ VOCALOIDParser/Effects/Effect.cs
@@ -21,6 +21,25 @@ namespace SixBeeps.VOCALOIDParser.Effects
         /// </summary>
         public bool Folded { get; set; }
 
+        internal static readonly Dictionary<string, Func<JsonNode, Effect>> EffectCreators = new() {
+            { "CDB4C488-BB24-45cb-8277-A245CB228BA8", (json) => new AutoPanEffect(json) },
+            { "F36904B7-5032-4cbe-AA48-C95440732F9D", (json) => new ChorusEffect(json) },
+            { "4D1F311D-DA90-459d-B581-003D003A1E5E", (json) => new CompressorEffect(json) },
+            { "BCE39D97-5E31-4bea-8FAB-C37BDD0FBAA4", (json) => new DeEsserEffect(json) },
+            { "5AEA0D66-9D3E-4a53-B46B-925C51B94499", (json) => new DelayEffect(json) },
+            { "9F301F02-A705-4b7b-9D51-E778E8338374", (json) => new DistortionEffect(json) },
+            { "C7E4ED13-FF1E-4fc2-a87c-65604789469D", (json) => new EqualizerEffect(json) },
+            { "C7CE3316-6FFD-4140-8a7c-0AA8B8108549", (json) => new GainEffect(json) },
+            { "30F75AB7-3B14-4439-A4B8-932A63A5F31E", (json) => new PhaserEffect(json) },
+            { "751EF2C0-4229-4ea7-AA0F-82EB5821D20E", (json) => new ReverbEffect(json) },
+            { "3213F12A-421C-43a8-B844-7FE8C28A0D64", (json) => new TremoloEffect(json) },
+            { "SingingSkill", (json) => new SingingSkillEffect(json) },
+            { "VoiceColor", (json) => new VoiceColorEffect(json) },
+            { "RobotVoice", (json) => new RobotVoiceEffect(json) },
+            { "DefaultLyric", (json) => new DefaultLyricEffect(json) },
+            { "Breath", (json) => new BreathEffect(json) }
+        };
+
         public Effect(JsonNode json)
         {
             paramList = json["parameters"].AsArray();
@@ -52,46 +71,11 @@ namespace SixBeeps.VOCALOIDParser.Effects
         /// <returns>A child class of <c>Effect</c> which represents the effect.</returns>
         public static Effect FromJsonEffect(JsonNode json)
         {
-            // Unfortunately, a dictionary can't be used here since types aren't compile-time constants and type conversion gets a little fuzzy.
-            switch (json["id"].GetValue<string>())
-            {
-                case "CDB4C488-BB24-45cb-8277-A245CB228BA8":
-                    return new AutoPanEffect(json);
-                case "F36904B7-5032-4cbe-AA48-C95440732F9D":
-                    return new ChorusEffect(json);
-                case "4D1F311D-DA90-459d-B581-003D003A1E5E":
-                    return new CompressorEffect(json);
-                case "BCE39D97-5E31-4bea-8FAB-C37BDD0FBAA4":
-                    return new DeEsserEffect(json);
-                case "5AEA0D66-9D3E-4a53-B46B-925C51B94499":
-                    return new DelayEffect(json);
-                case "9F301F02-A705-4b7b-9D51-E778E8338374":
-                    return new DistortionEffect(json);
-                case "C7E4ED13-FF1E-4fc2-a87c-65604789469D":
-                    return new EqualizerEffect(json);
-                case "C7CE3316-6FFD-4140-8a7c-0AA8B8108549":
-                    return new GainEffect(json);
-                case "30F75AB7-3B14-4439-A4B8-932A63A5F31E":
-                    return new PhaserEffect(json);
-                case "751EF2C0-4229-4ea7-AA0F-82EB5821D20E":
-                    return new ReverbEffect(json);
-                case "3213F12A-421C-43a8-B844-7FE8C28A0D64":
-                    return new TremoloEffect(json);
-
-                case "SingingSkill":
-                    return new SingingSkillEffect(json);
-                case "VoiceColor":
-                    return new VoiceColorEffect(json);
-                case "RobotVoice":
-                    return new RobotVoiceEffect(json);
-                case "DefaultLyric":
-                    return new DefaultLyricEffect(json);
-                case "Breath":
-                    return new BreathEffect(json);
-
-                default:
-                    return new FallbackEffect(json);
+            if (EffectCreators.TryGetValue(json["id"].GetValue<string>(), out var createEffect)) {
+                return createEffect.Invoke(json);
             }
+
+            return new FallbackEffect(json);
         }
 
         protected T GetParamValueByName<T>(string name)
