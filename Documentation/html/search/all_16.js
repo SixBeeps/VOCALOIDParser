82ee9309caed2e42c2776d6f801b815b828d52f1
Brandon Lee <brandononlyu@gmail.com>
2024-05-30 03:10:04 -0400

    Update Doxyfile to generate into /docs

--- Documentation/html/search/all_16.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['wavefile_0',['WaveFile',['../d3/db4/struct_six_beeps_1_1_v_o_c_a_l_o_i_d_parser_1_1_audio_event.html#a6c2691eb01bcb15c1187e030cfa81ffe',1,'SixBeeps::VOCALOIDParser::AudioEvent']]],
-  ['waveshape_1',['WaveShape',['../d9/d7f/class_six_beeps_1_1_v_o_c_a_l_o_i_d_parser_1_1_effects_1_1_auto_pan_effect.html#ac8d3643c2920aaeb011edc0c555a62d9',1,'SixBeeps.VOCALOIDParser.Effects.AutoPanEffect.WaveShape'],['../d8/da6/class_six_beeps_1_1_v_o_c_a_l_o_i_d_parser_1_1_effects_1_1_tremolo_effect.html#a7208c35eaf9f9c50a802742d62504247',1,'SixBeeps.VOCALOIDParser.Effects.TremoloEffect.WaveShape']]],
-  ['wavetesttrack_2',['WaveTestTrack',['../d4/d0f/class_unit_testing_1_1_unit_test1.html#af5e932d70e4edeea878f94bf7d76146c',1,'UnitTesting::UnitTest1']]],
-  ['what_20vocaloidparser_20can_20and_20can_20t_20do_3',['What VOCALOIDParser can and can&apos;t do',['../d3/dcc/md__r_e_a_d_m_e.html#autotoc_md2',1,'']]],
-  ['word_4',['Word',['../da/dc7/struct_six_beeps_1_1_v_o_c_a_l_o_i_d_parser_1_1_lyric.html#a0e2bbeaf5371d94ab6f5df98e42c2e81',1,'SixBeeps::VOCALOIDParser::Lyric']]]
-];
--- Documentation/html/search/all_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['id_0',['Id',['../db/dd5/class_six_beeps_1_1_v_o_c_a_l_o_i_d_parser_1_1_d_v_q_m.html#ad0d98bb0c66e3ba62af2994fe61bf14d',1,'SixBeeps.VOCALOIDParser.DVQM.Id'],['../d3/ddc/class_six_beeps_1_1_v_o_c_a_l_o_i_d_parser_1_1_effects_1_1_effect.html#a4808185ee50fc30fce6e43f671d68869',1,'SixBeeps.VOCALOIDParser.Effects.Effect.Id']]],
-  ['installation_20and_20usage_1',['Installation and Usage',['../d3/dcc/md__r_e_a_d_m_e.html#autotoc_md3',1,'']]],
-  ['ivocaloidevent_2',['IVocaloidEvent',['../d8/dae/interface_six_beeps_1_1_v_o_c_a_l_o_i_d_parser_1_1_i_vocaloid_event.html',1,'SixBeeps::VOCALOIDParser']]]
-];
